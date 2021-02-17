07982bb0687fb2d6d9cd8e9879621f795386ae93
HDzzzz <40918889+HDzzzz@users.noreply.github.com>
2021-02-17 02:14:59 +0000

    Add files via upload

---
+++ C# Hcaptcha solver example/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace Example_HcaptchaSolver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.thirteenForm1 = new Teen.ThirteenForm();
+            this.thirteenControlBox1 = new Teen.ThirteenControlBox();
+            this.thirteenButton1 = new Teen.ThirteenButton();
+            this.thirteenButton2 = new Teen.ThirteenButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.thirteenTextBox1 = new Teen.ThirteenTextBox();
+            this.thirteenTextBox2 = new Teen.ThirteenTextBox();
+            this.thirteenTextBox3 = new Teen.ThirteenTextBox();
+            this.thirteenForm1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // thirteenForm1
+            // 
+            this.thirteenForm1.AccentColor = System.Drawing.Color.IndianRed;
+            this.thirteenForm1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.thirteenForm1.ColorScheme = Teen.ThirteenForm.ColorSchemes.Dark;
+            this.thirteenForm1.Controls.Add(this.thirteenTextBox3);
+            this.thirteenForm1.Controls.Add(this.thirteenTextBox2);
+            this.thirteenForm1.Controls.Add(this.thirteenTextBox1);
+            this.thirteenForm1.Controls.Add(this.listBox1);
+            this.thirteenForm1.Controls.Add(this.thirteenButton2);
+            this.thirteenForm1.Controls.Add(this.thirteenButton1);
+            this.thirteenForm1.Controls.Add(this.thirteenControlBox1);
+            this.thirteenForm1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.thirteenForm1.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F);
+            this.thirteenForm1.ForeColor = System.Drawing.Color.White;
+            this.thirteenForm1.Location = new System.Drawing.Point(0, 0);
+            this.thirteenForm1.Name = "thirteenForm1";
+            this.thirteenForm1.Size = new System.Drawing.Size(277, 394);
+            this.thirteenForm1.TabIndex = 0;
+            this.thirteenForm1.Text = "Hcaptcha | Nullcheats";
+            // 
+            // thirteenControlBox1
+            // 
+            this.thirteenControlBox1.AccentColor = System.Drawing.Color.IndianRed;
+            this.thirteenControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.thirteenControlBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.thirteenControlBox1.ColorScheme = Teen.ThirteenControlBox.ColorSchemes.Dark;
+            this.thirteenControlBox1.ForeColor = System.Drawing.Color.White;
+            this.thirteenControlBox1.Location = new System.Drawing.Point(175, 2);
+            this.thirteenControlBox1.Name = "thirteenControlBox1";
+            this.thirteenControlBox1.Size = new System.Drawing.Size(100, 25);
+            this.thirteenControlBox1.TabIndex = 0;
+            this.thirteenControlBox1.Text = "thirteenControlBox1";
+            // 
+            // thirteenButton1
+            // 
+            this.thirteenButton1.AccentColor = System.Drawing.Color.IndianRed;
+            this.thirteenButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.thirteenButton1.ColorScheme = Teen.ThirteenButton.ColorSchemes.Dark;
+            this.thirteenButton1.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F);
+            this.thirteenButton1.ForeColor = System.Drawing.Color.White;
+            this.thirteenButton1.Location = new System.Drawing.Point(135, 359);
+            this.thirteenButton1.Name = "thirteenButton1";
+            this.thirteenButton1.Size = new System.Drawing.Size(130, 23);
+            this.thirteenButton1.TabIndex = 1;
+            this.thirteenButton1.Text = "Copy captcha";
+            this.thirteenButton1.UseVisualStyleBackColor = false;
+            this.thirteenButton1.Click += new System.EventHandler(this.thirteenButton1_Click);
+            // 
+            // thirteenButton2
+            // 
+            this.thirteenButton2.AccentColor = System.Drawing.Color.IndianRed;
+            this.thirteenButton2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.thirteenButton2.ColorScheme = Teen.ThirteenButton.ColorSchemes.Dark;
+            this.thirteenButton2.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F);
+            this.thirteenButton2.ForeColor = System.Drawing.Color.White;
+            this.thirteenButton2.Location = new System.Drawing.Point(12, 359);
+            this.thirteenButton2.Name = "thirteenButton2";
+            this.thirteenButton2.Size = new System.Drawing.Size(128, 23);
+            this.thirteenButton2.TabIndex = 2;
+            this.thirteenButton2.Text = "Solve captcha";
+            this.thirteenButton2.UseVisualStyleBackColor = false;
+            this.thirteenButton2.Click += new System.EventHandler(this.thirteenButton2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.ForeColor = System.Drawing.Color.White;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 17;
+            this.listBox1.Location = new System.Drawing.Point(12, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(253, 221);
+            this.listBox1.TabIndex = 3;
+            // 
+            // thirteenTextBox1
+            // 
+            this.thirteenTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.thirteenTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.thirteenTextBox1.ColorScheme = Teen.ThirteenTextBox.ColorSchemes.Dark;
+            this.thirteenTextBox1.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F);
+            this.thirteenTextBox1.ForeColor = System.Drawing.Color.White;
+            this.thirteenTextBox1.Location = new System.Drawing.Point(12, 266);
+            this.thirteenTextBox1.Name = "thirteenTextBox1";
+            this.thirteenTextBox1.Size = new System.Drawing.Size(253, 25);
+            this.thirteenTextBox1.TabIndex = 4;
+            this.thirteenTextBox1.Text = "API key";
+            this.thirteenTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // thirteenTextBox2
+            // 
+            this.thirteenTextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.thirteenTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.thirteenTextBox2.ColorScheme = Teen.ThirteenTextBox.ColorSchemes.Dark;
+            this.thirteenTextBox2.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F);
+            this.thirteenTextBox2.ForeColor = System.Drawing.Color.White;
+            this.thirteenTextBox2.Location = new System.Drawing.Point(12, 297);
+            this.thirteenTextBox2.Name = "thirteenTextBox2";
+            this.thirteenTextBox2.Size = new System.Drawing.Size(253, 25);
+            this.thirteenTextBox2.TabIndex = 5;
+            this.thirteenTextBox2.Text = "Hcaptcha site key";
+            this.thirteenTextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // thirteenTextBox3
+            // 
+            this.thirteenTextBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.thirteenTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.thirteenTextBox3.ColorScheme = Teen.ThirteenTextBox.ColorSchemes.Dark;
+            this.thirteenTextBox3.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F);
+            this.thirteenTextBox3.ForeColor = System.Drawing.Color.White;
+            this.thirteenTextBox3.Location = new System.Drawing.Point(12, 328);
+            this.thirteenTextBox3.Name = "thirteenTextBox3";
+            this.thirteenTextBox3.Size = new System.Drawing.Size(253, 25);
+            this.thirteenTextBox3.TabIndex = 6;
+            this.thirteenTextBox3.Text = "https://www.website.com";
+            this.thirteenTextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(277, 394);
+            this.Controls.Add(this.thirteenForm1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.thirteenForm1.ResumeLayout(false);
+            this.thirteenForm1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Teen.ThirteenForm thirteenForm1;
+        private Teen.ThirteenControlBox thirteenControlBox1;
+        private Teen.ThirteenTextBox thirteenTextBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private Teen.ThirteenButton thirteenButton2;
+        private Teen.ThirteenButton thirteenButton1;
+        private Teen.ThirteenTextBox thirteenTextBox3;
+        private Teen.ThirteenTextBox thirteenTextBox2;
+    }
+}
+
---
+++ C# Hcaptcha solver example/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example_HcaptchaSolver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+        private static string Result;
+        private void SolveHcaptcha()
+        {
+            HcaptchaSolverNET.Solver Hsolver = new HcaptchaSolverNET.Solver();
+            Hsolver.apikey = thirteenTextBox1.Text;
+            string SiteKey = thirteenTextBox2.Text;
+            string Domain = thirteenTextBox3.Text;
+
+            if ((!string.IsNullOrEmpty(Hsolver.apikey)) && (!string.IsNullOrEmpty(SiteKey)) && (!string.IsNullOrEmpty(Domain)))
+            {
+                listBox1.Items.Add("Fields validated");
+                listBox1.Items.Add("Attempting to get 2captcha ID");
+                string ID = Hsolver.GetCaptchaToken(SiteKey, Domain, out bool IDstatus);
+                if (IDstatus == true)
+                {
+                    listBox1.Items.Add("2cap task ID = " + ID);
+                    listBox1.Items.Add("Attempting to get captcha result / solve");
+                    string CaptchaValue = Hsolver.GetSolvedCaptchaID(ID, out bool CaptchaSolvedResult);
+                    if (CaptchaSolvedResult == true)
+                    {
+                        listBox1.Items.Add("Got value = " + CaptchaValue);
+                        Result = CaptchaValue;
+                    }
+                    else
+                    {
+                        listBox1.Items.Add("Failed to grab result of captcha");
+                    }
+                }
+                else
+                {
+                    listBox1.Items.Add("Failed to grab 2captcha task ID");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please validate input fields - error", "Error");
+            }
+        }
+        private void thirteenButton2_Click(object sender, EventArgs e)
+        {
+            Thread Hcap = new Thread(SolveHcaptcha);
+            Hcap.Start();
+        }
+
+        private void thirteenButton1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(Result);
+        }
+    }
+}
